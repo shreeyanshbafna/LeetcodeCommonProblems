0d0c245e8ffac0639bb19096ab93b71350c26b91
shreeyanshbafna <shreeyanshbafna@gmail.com>
2025-11-12 20:11:51 +0530

    Binary search common methods

--- Leetcode/BST1.java
+++ Leetcode/BST1.java
@@ -33,12 +33,105 @@ class Bst {
 				root.left = insert(root.left, data);
 
 			} else if (root.data < data) {
-				root.right = insert(root.left, data);
+				root.right = insert(root.right, data);
 
 			}
 		}
 		return root;
 	}
+
+	Node delete(Node root, int data) {
+		if (root == null)
+			return null;
+		if (data < root.data) {
+		    root.left = delete(root.left, data);
+		} else if (data > root.data) {
+		    root.right = delete(root.right, data);
+		} else {
+			 if (root.left == null) {
+				return root.right;
+			} else if (root.right == null) {
+				return root.left;
+			}
+			Node minNode = findMinNode(root.right);
+			root.data = minNode.data;
+			root.right = delete(root.right, minNode.data);
+
+		}
+		return root;
+	}
+
+	Node findMinNode(Node root) {
+		while (root.left != null) {
+			root = root.left;
+
+		}
+		;
+		return root;
+	}
+
+	boolean search(Node root, int data) {
+		if (root == null)
+			return false;
+		else if (root.data == data) {
+			return true;
+
+		} else {
+			if (root.data > data) {
+				return search(root.left, data);
+
+			} else if (root.data < data) {
+				return search(root.right, data);
+
+			}
+		}
+		return false;
+	}
+
+	// left to root to right
+	void inorder(Node root) {
+		if (root != null) {
+			inorder(root.left);
+			System.out.print(root.data + " ");
+			inorder(root.right);
+		}
+	}
+
+	// root ->left -> right
+	void preorder(Node root) {
+		if (root != null) {
+			System.out.print(root.data + " ");
+			preorder(root.left);
+			preorder(root.right);
+
+		}
+	}
+
+	// left -> right -> root
+	void postorder(Node root) {
+		if (root != null) {
+			postorder(root.left);
+			postorder(root.right);
+			System.out.print(root.data + " ");
+		}
+	}
+
+    // ✅ Print Tree in readable way
+    void printTree(Node root, String prefix, boolean isLeft) {
+        if (root == null) return;
+
+        if (root.right != null)
+            printTree(root.right, prefix + (isLeft ? "│   " : "    "), false);
+
+        System.out.println(prefix + (isLeft ? "└── " : "┌── ") + root.data);
+
+        if (root.left != null)
+            printTree(root.left, prefix + (isLeft ? "    " : "│   "), true);
+    }
+
+    void print() {
+        printTree(root, "", true);
+    }
 
 }
 
@@ -56,7 +149,20 @@ public class BST1 {
 		tree.root.right.right = new Node(14);
 		tree.root.right.right.left = new Node(13);
 		System.out.println(tree);
+		System.out.println("Height of tree: " + tree.height(tree.root));
 
+		System.out.println("Search 6: " + tree.search(tree.root, 6));
+		System.out.println("Search 9: " + tree.search(tree.root, 9));
+		System.out.println();
+		tree.preorder(tree.root);
+		System.out.println();
+		tree.print();
+		System.out.println();
+		tree.delete(tree.root, 10);
+		System.out.println();
+		tree.preorder(tree.root);
+		System.out.println();
+tree.print();
 	}
 
 }
